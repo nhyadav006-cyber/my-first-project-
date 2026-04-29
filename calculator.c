0df594cc693d61f361e01bc874db884f4366f729
nhyadav006-cyber <nhyadav006@gmail.com>
2026-04-29 23:08:29 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(){
+	int choice;
+	float a, b;
+	
+	printf("calculator Menu\n");
+	printf("1. Addition\n");
+	printf("2. Multiplication\n");
+		printf("3. Subtraction\n");
+			printf("4.Division\n");
+			printf("5. percentage\n");
+			
+			printf("Enter your choice ");
+			scanf("%d",&choice);
+			
+			printf("Enter the two number "); // for average a= obtained number and b= total number//
+			scanf("%f %f", &a, &b);
+			
+			switch (choice){
+				case 1:
+					printf(" Addition = %.2f", a + b);
+					 break;	
+					 
+					 	case 2:
+					printf(" Multiplication = %.2f", a * b);
+					 break;	
+					 
+					  	case 3:
+					printf("Subtraction = %.2f", a - b);
+					 break;	
+					 
+					 	  	case 4:
+					 	  		if(b!= 0)
+					printf(" Division = %.2f", a / b);
+					else
+					printf("Division by zero is not allowed");
+					 break;	
+					 
+					 	 default:
+					 printf("Invalide choice");
+					 	}
+					 	return 0;
+}
